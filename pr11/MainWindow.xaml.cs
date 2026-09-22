f500ba41293a412e0172047567285343caedf082
Хонор <@LAPTOP-15FUL4PU>
2026-09-22 23:39:41 +0500

    Создание таймера и метода AttackPlayer. Патрушева

--- pr11/MainWindow.xaml.cs
+++ pr11/MainWindow.xaml.cs
@@ -13,7 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Collections.Generic;
+using System.Windows.Threading;
 
 
 namespace pr11
@@ -23,12 +24,31 @@ namespace pr11
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public PersonInfo Player = new PersonInfo("Student", 100, 10, 1,0, 0, 5);
+        public List<PersonInfo> Enemys = new List<PersonInfo>();
+        DispatcherTimer dispatherTimer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            UserInfoPlayr();
+
+            Enemys.Add(new PersonInfo("Большой монстр", 150, 10, 1, 0, 15, 10));
+            Enemys.Add(new PersonInfo("Средний монстр", 120, 15, 1, 0, 30, 15));
+            Enemys.Add(new PersonInfo("Маленький монстр", 100, 20, 1, 0, 40, 20));
+
+            dispatherTimer.Tick += AttackPlayer;
+            dispatherTimer.Interval = new System.TimeSpan(0, 0, 10);
+            dispatherTimer.Start();
         }
-        
+
+        private void AttackPlayer(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
          public void UserInfoPlayr()
         {
             if (Player.Glasses > Player.Level * 100)
@@ -46,6 +66,9 @@ namespace pr11
             playerMoney.Content = "Монеты: " + Player.Mony;
         }
 
-       
+        private void AttackEnemy(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
